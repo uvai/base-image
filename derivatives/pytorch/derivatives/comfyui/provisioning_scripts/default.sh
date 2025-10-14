1491b1048ffb390bafe4081f1b09be88c4206401
UVAI <38521971+uvai@users.noreply.github.com>
2025-10-14 13:53:07 +1100

    Update default.sh

--- derivatives/pytorch/derivatives/comfyui/provisioning_scripts/default.sh
+++ derivatives/pytorch/derivatives/comfyui/provisioning_scripts/default.sh
@@ -27,6 +27,7 @@ NODES=(
     "https://github.com/numz/ComfyUI-SeedVR2_VideoUpscaler.git"
     "https://github.com/kijai/ComfyUI-GIMM-VFI.git"
     "https://github.com/BigStationW/ComfyUi-Scale-Image-to-Total-Pixels-Advanced.git"
+    "https://github.com/moonwhaler/comfyui-seedvr2-tilingupscaler.git"
     
     
 )
