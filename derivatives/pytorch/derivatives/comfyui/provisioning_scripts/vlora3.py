5f3e58fa04849a919b4ea9c051b0e54949c87171
UVAI <38521971+uvai@users.noreply.github.com>
2026-07-13 13:38:20 +1000

    Update vlora3.py

--- derivatives/pytorch/derivatives/comfyui/provisioning_scripts/vlora3.py
+++ derivatives/pytorch/derivatives/comfyui/provisioning_scripts/vlora3.py
@@ -1,15 +1,11 @@
 import sys
 import subprocess
-import pkg_resources
 
 # ─────────────────────────────────────────────
 # Auto-install missing packages
 # ─────────────────────────────────────────────
-required = {"google-api-python-client", "google-auth", "gdown", "huggingface_hub", "tqdm"}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
-if missing:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", *missing])
+required = ["google-api-python-client", "google-auth", "gdown", "huggingface_hub", "tqdm"]
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--quiet", *required])
 
 import os
 import io
